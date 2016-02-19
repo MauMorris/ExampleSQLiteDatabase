7c4a6372c385be10640a2fd2411711ed2f4af713
mauriciogodinez <mgdinezp@gmail.com>
2016-02-19 07:28:29 -0600

    without regex

--- app/src/main/java/com/example/mauriciogodinez/basedatos1/DatabaseHelper.java
+++ app/src/main/java/com/example/mauriciogodinez/basedatos1/DatabaseHelper.java
@@ -1,6 +1,8 @@
 package com.example.mauriciogodinez.basedatos1;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -18,7 +20,6 @@ public class DatabaseHelper extends SQLiteOpenHelper{
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
-        SQLiteDatabase db = this.getWritableDatabase();
     }
 
     @Override
@@ -31,5 +32,39 @@ public class DatabaseHelper extends SQLiteOpenHelper{
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXIST " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean insertData(String name, String lastName, String marks){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2, name);
+        contentValues.put(COL_3, lastName);
+        contentValues.put(COL_4, marks);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        return result == -1 ? false : true;
+    }
+
+    public Cursor getAllData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from " + TABLE_NAME, null);
+        return res;
+    }
+
+    public boolean upDateDataNow(String id, String name, String lastName, String marks){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_1, id);
+        contentValues.put(COL_2, name);
+        contentValues.put(COL_3, lastName);
+        contentValues.put(COL_4, marks);
+        db.update(TABLE_NAME, contentValues, "ID = ?", new String[] { id});
+        return true;
+    }
+
+    public Integer deleteDataNow(String id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_NAME, "ID = ?", new String[] {id});
     }
 }
--- app/src/main/java/com/example/mauriciogodinez/basedatos1/MainActivity.java
+++ app/src/main/java/com/example/mauriciogodinez/basedatos1/MainActivity.java
@@ -1,15 +1,118 @@
 package com.example.mauriciogodinez.basedatos1;
 
+import android.app.AlertDialog;
+import android.database.Cursor;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
+
     DatabaseHelper myDb;
+
+    protected EditText editName;
+    protected EditText editLastName;
+    protected EditText editMarks;
+    protected EditText editId;
+
+    protected Button addData;
+    protected Button showData;
+    protected Button upDateData;
+    protected Button deleteData;
+
+    boolean isInserted;
+
+    String pattern = "(.*)(\\d+)(.*)";
+    String line = "This order was placed for QT3000! OK?";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         myDb = new DatabaseHelper(this);
+
+        editName = (EditText) findViewById(R.id.editText_name);
+        editLastName = (EditText) findViewById(R.id.editText_lastName);
+        editMarks = (EditText) findViewById(R.id.editText_marks);
+        editId = (EditText) findViewById(R.id.editText_id);
+
+        addData = (Button) findViewById(R.id.button_addData);
+        showData = (Button) findViewById(R.id.button_showData);
+        upDateData = (Button) findViewById(R.id.button_upDateData);
+        deleteData = (Button) findViewById(R.id.button_deleteData);
+
+        Pattern r = Pattern.compile(pattern);
+        Matcher m = r.matcher(line);
     }
+
+    public void addData(View v){
+        isInserted = myDb.insertData(editName.getText().toString(),
+                    editLastName.getText().toString(),
+                        editMarks.getText().toString());
+        if (isInserted)
+            Toast.makeText(MainActivity.this, "Data inserted", Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(MainActivity.this, "Data no inserted", Toast.LENGTH_SHORT).show();
+
+        editName.setText("");
+        editLastName.setText("");
+        editMarks.setText("");
+    }
+
+    public void showData(View v){
+        Cursor res = myDb.getAllData();
+        if(res.getCount() == 0) {
+            showMessage("Error", "Nothing found");
+            return;
+        }
+        StringBuffer buffer = new StringBuffer();
+        while (res.moveToNext()){
+            buffer.append("Id :" + res.getString(0) + "\n");
+            buffer.append("Name :" + res.getString(1) + "\n");
+            buffer.append("LastName :" + res.getString(2) + "\n");
+            buffer.append("Marks :" + res.getString(3) + "\n\n");
+        }
+        showMessage("Data", buffer.toString());
+    }
+
+    public void upDateData(View v){
+        boolean isUpdate = myDb.upDateDataNow(editId.getText().toString(),
+                editName.getText().toString(),
+                editLastName.getText().toString(),
+                editMarks.getText().toString());
+        if(isUpdate)
+            Toast.makeText(MainActivity.this, "Data upDated", Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(MainActivity.this, "Data no upDated", Toast.LENGTH_SHORT).show();
+
+        editName.setText("");
+        editLastName.setText("");
+        editMarks.setText("");
+        editId.setText("");
+    }
+
+    public void deleteData(View v){
+        Integer deletedRows = myDb.deleteDataNow(editId.getText().toString());
+        if (deletedRows >0)
+            Toast.makeText(MainActivity.this, "Data deleted", Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(MainActivity.this, "Data no deleted", Toast.LENGTH_SHORT).show();
+    }
+
+    public void showMessage(String title, String message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
+    }
+
 }
