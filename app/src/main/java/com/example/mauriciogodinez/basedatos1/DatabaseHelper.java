9451b8d1df6a18a78b71940743ce0988cb26ebb6
mauriciogodinez <mgdinezp@gmail.com>
2017-03-18 21:27:55 -0600

    Se eliminan las regex y modifica la UI con botones dentro de un TableLayout

--- app/src/main/java/com/example/mauriciogodinez/basedatos1/DatabaseHelper.java
+++ app/src/main/java/com/example/mauriciogodinez/basedatos1/DatabaseHelper.java
@@ -6,17 +6,14 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-/**
- * Created by mauriciogodinez on 14/02/16.
- */
 public class DatabaseHelper extends SQLiteOpenHelper{
 
-    public static final String DATABASE_NAME = "Student.db";
-    public static final String TABLE_NAME = "student_table";
-    public static final String COL_1 = "ID";
-    public static final String COL_2 = "NAME";
-    public static final String COL_3 = "LASTNAME";
-    public static final String COL_4 = "MARKS";
+    private static final String DATABASE_NAME = "Student.db";
+    private static final String TABLE_NAME = "student_table";
+    private static final String COL_1 = "ID";
+    private static final String COL_2 = "NAME";
+    private static final String COL_3 = "LASTNAME";
+    private static final String COL_4 = "MARKS";
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
@@ -24,9 +21,13 @@ public class DatabaseHelper extends SQLiteOpenHelper{
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " + TABLE_NAME +
-                " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                "NAME TEXT, LASTNAME TEXT, MARKS INTEGER)");
+        String CREATE_TABLE = "create table " + TABLE_NAME + " (" +
+                COL_1 + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COL_2 + " TEXT, " +
+                COL_3 + " TEXT, " +
+                COL_4 + " INTEGER)";
+
+        db.execSQL(CREATE_TABLE);
     }
 
     @Override
@@ -37,29 +38,36 @@ public class DatabaseHelper extends SQLiteOpenHelper{
 
     public boolean insertData(String name, String lastName, String marks){
         SQLiteDatabase db = this.getWritableDatabase();
+
         ContentValues contentValues = new ContentValues();
+
         contentValues.put(COL_2, name);
         contentValues.put(COL_3, lastName);
         contentValues.put(COL_4, marks);
 
         long result = db.insert(TABLE_NAME, null, contentValues);
-        return result == -1 ? false : true;
+        return result != -1;
     }
 
     public Cursor getAllData(){
         SQLiteDatabase db = this.getWritableDatabase();
+
         Cursor res = db.rawQuery("select * from " + TABLE_NAME, null);
         return res;
     }
 
     public boolean upDateDataNow(String id, String name, String lastName, String marks){
         SQLiteDatabase db = this.getWritableDatabase();
+
         ContentValues contentValues = new ContentValues();
+
         contentValues.put(COL_1, id);
         contentValues.put(COL_2, name);
         contentValues.put(COL_3, lastName);
         contentValues.put(COL_4, marks);
+
         db.update(TABLE_NAME, contentValues, "ID = ?", new String[] { id});
+
         return true;
     }
 
